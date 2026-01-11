18f1556b6a01c6d36fb1b038802f6b860b78be34
Andrés Banda <andrezh_gln7@live.com>
2026-01-10 21:38:27 -0500

    chore(primitives): add barrel exports

--- src/primitives/Checkbox/Checkbox.test.tsx
+++ src/primitives/Checkbox/Checkbox.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { describe, it, expect } from "vitest";
 
-import Label from "../Label/Label";
+import { Label } from "../Label";
 import Checkbox from "./Checkbox";
 
 describe("Checkbox", () => {
